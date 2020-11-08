d69ab93dacf11e2b142b8dcaa57d335563ab1c3a
Ajay Sharma <ajashar3@in.ibm.com>
2020-11-08 15:10:12 +0530

    Initial Commit

---
+++ ExampleCICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleCICD
+//
+//  Created by Ajay Sharma on 08/11/20.
+//  Copyright © 2020 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
